5b4da9d9d97ead46512a610e9dd9d5c7a5543cee
August Skarsfjord Nyheim <auny@computas.com>
2025-06-17 14:08:31 +0200

    Oppgave 1 backend

--- backend/Services/CvService.cs
+++ backend/Services/CvService.cs
@@ -14,6 +14,11 @@ public class CvService(AppDbContext context) : ICvService
 
     // TODO: Oppgave 1
 
+    public async Task<User?> GetUserByIdAsync(Guid id)
+    {
+        return await context.Users.FindAsync(id);
+    }
+
     public async Task<IEnumerable<Experience>> GetAllExperiencesAsync()
     {
         // TODO: Oppgave 2
